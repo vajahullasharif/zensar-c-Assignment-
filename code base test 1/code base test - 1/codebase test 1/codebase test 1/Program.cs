b488b1153375f9aaf02e6b2df6309b97985adda5
vajahullasharif <vajahullamohmmed@gmail.com>
2022-05-05 09:38:05 +0530

    codetest -1

---
+++ code base test 1/code base test - 1/codebase test 1/codebase test 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codebase_test_1
+
+{
+    class Passenger
+    {
+        string name;
+        string DateofTravel;
+        int Age;
+        Passenger(string name, string DateofTravel, int Age)
+        {
+            this.name = name;
+            this.DateofTravel = DateofTravel;
+            this.Age = Age;
+        }
+
+        public void TicketBooking(int no_of_tickets)
+        {
+            if (no_of_tickets > 5)
+            {
+                throw (new TicketsException("No more than 5 tickets"));
+            }
+            else
+            {
+                Console.WriteLine("Tickets successfully: {0}", no_of_tickets);
+
+            }
+        }
+
+public class TicketsException : Exception
+        {
+            public TicketsException(string message) : base(message)
+            {
+            }
+        }
+
+        public static void Main()
+        {
+            int no_tickets;
+            no_tickets = Convert.ToInt32(Console.Read());
+            Console.WriteLine(no_tickets);
+            Passenger pass = new Passenger("Vajahulla", "5-05-2022", 22);
+            pass.TicketBooking(no_tickets);
+        }
+    }
+}
+
+
+    
+   
+
+
+        
+    
+
